7a66def22c017c59f2055f65d80260f1b3032dd4
Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
2024-09-30 19:35:36 +0200

    Fix error when git is missing

--- swattool/utils.py
+++ swattool/utils.py
@@ -4,11 +4,23 @@
 
 import pathlib
 import subprocess
+from typing import Optional
+
 import xdg
 
 BINDIR = pathlib.Path(__file__).parent.parent.resolve()
 DATADIR = xdg.xdg_cache_home() / "swattool"
 CACHEDIR = DATADIR / "cache"
 
-MAILNAME = subprocess.run(["git", "config", "--global", "user.name"],
-                          capture_output=True).stdout.decode().strip()
+
+def _get_git_username() -> Optional[str]:
+    try:
+        process = subprocess.run(["git", "config", "--global", "user.name"],
+                                 capture_output=True, check=True)
+    except Exception:
+        return None
+
+    return process.stdout.decode().strip()
+
+
+MAILNAME = _get_git_username()
