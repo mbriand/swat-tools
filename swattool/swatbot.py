7d755bee71840a7dbb6e0f5e2758d3ad128992f0
Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
2024-11-06 16:52:52 +0100

    swatbuild: Allow concurrent constructions of build data

--- swattool/swatbot.py
+++ swattool/swatbot.py
@@ -2,6 +2,7 @@
 
 """Interraction with the swatbot Django server."""
 
+import concurrent.futures
 import logging
 from typing import Any, Collection
 
@@ -28,8 +29,9 @@ def get_failure_infos(limit: int, sort: Collection[str],
     logger.info("Loading build failures details...")
     infos = []
     limited_pending_ids = sorted(failures.keys(), reverse=True)[:limit]
-    with click.progressbar(limited_pending_ids) as pending_ids_progress:
-        for buildid in pending_ids_progress:
+    jobs = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        for buildid in limited_pending_ids:
             # Filter on status now, limiting the size of data we will have to
             # download from the server.
             if filters['triage']:
@@ -39,10 +41,23 @@ def get_failure_infos(limit: int, sort: Collection[str],
                 if triages.isdisjoint(filters['triage']):
                     continue
 
-            build = swatbuild.Build(buildid, failures[buildid])
+            jobs.append(executor.submit(swatbuild.Build, buildid,
+                                        failures[buildid]))
 
-            if build.match_filters(filters, userinfos[build.id]):
-                infos.append(build)
+        try:
+            complete_iterator = concurrent.futures.as_completed(jobs)
+            with click.progressbar(complete_iterator,
+                                   length=len(jobs)) as jobsprogress:
+                for future in jobsprogress:
+                    build = future.result()
+                    if build.match_filters(filters, userinfos[build.id]):
+                        infos.append(build)
+        except KeyboardInterrupt:
+            executor.shutdown(cancel_futures=True)
+            return ([], userinfos)
+        except Exception:
+            executor.shutdown(cancel_futures=True)
+            raise
 
     def sortfn(elem):
         return elem.get_sort_tuple([swatbuild.Field(k) for k in sort])
