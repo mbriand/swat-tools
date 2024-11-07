6f4b3a58e8b9f33f8ef3dddacbda0b82f98b3552
Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
2024-11-07 16:23:47 +0100

    swatbot: code cleaning
    Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>

--- swattool/swatbot.py
+++ swattool/swatbot.py
@@ -15,25 +15,17 @@ from . import userdata
 logger = logging.getLogger(__name__)
 
 
-def get_failure_infos(limit: int, sort: Collection[str],
-                      filters: dict[str, Any]
-                      ) -> tuple[list[swatbuild.Build], userdata.UserInfos]:
-    """Get consolidated list of failure infos and local reviews infos."""
-    userinfos = userdata.UserInfos()
-
-    logger.info("Loading build failures...")
-
-    statusfilter = None
-    if len(filters.get('triage', [])) == 1:
-        statusfilter = filters['triage'][0]
-    failures = swatbotrest.get_failures(statusfilter)
-
-    # Generate a list of all pending failures, fetching details from the remote
-    # server as needed.
+def _create_builds(filters: dict[str, Any],
+                   failures: dict[int, dict[int, dict]],
+                   limit: int,
+                   userinfos: userdata.UserInfos):
     logger.info("Loading build failures details...")
     infos = []
     limited_pending_ids = sorted(failures.keys(), reverse=True)[:limit]
     jobs = []
+
+    # Generate a list of all pending failures, fetching details from the remote
+    # server as needed.
     with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
         for buildid in limited_pending_ids:
             # Filter on status now, limiting the size of data we will have to
@@ -62,6 +54,24 @@ def get_failure_infos(limit: int, sort: Collection[str],
         except Exception:
             executor.shutdown(cancel_futures=True)
             raise
+
+    return infos
+
+
+def get_failure_infos(limit: int, sort: Collection[str],
+                      filters: dict[str, Any]
+                      ) -> tuple[list[swatbuild.Build], userdata.UserInfos]:
+    """Get consolidated list of failure infos and local reviews infos."""
+    userinfos = userdata.UserInfos()
+
+    logger.info("Loading build failures...")
+
+    statusfilter = None
+    if len(filters.get('triage', [])) == 1:
+        statusfilter = filters['triage'][0]
+    failures = swatbotrest.get_failures(statusfilter)
+
+    infos = _create_builds(filters, failures, limit, userinfos)
 
     def sortfn(elem):
         return elem.get_sort_tuple([swatbuild.Field(k) for k in sort])
