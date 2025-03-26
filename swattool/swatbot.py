9f2c3ea1e9f7a11cbabc5eadc35cfb6d1fa46996
Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
2025-03-26 17:35:52 +0100

    all: Rework parallel works on init
    Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>

--- swattool/swatbot.py
+++ swattool/swatbot.py
@@ -2,112 +2,78 @@
 
 """Interraction with the swatbot Django server."""
 
-import concurrent.futures
 import logging
-from typing import Any, Collection, Optional
-
-import click
+from typing import Any, Collection
 
 from . import logsview
 from . import swatbotrest
 from . import swatbuild
 from . import userdata
+from . import utils
 
 logger = logging.getLogger(__name__)
 
 
-def _create_build(filters: dict[str, Any],
-                  buildid: int,
-                  failures: dict[int, dict],
-                  userinfos: userdata.UserInfos
-                  ) -> tuple[str, Optional[swatbuild.Build]]:
-    build = swatbuild.Build(buildid, failures)
+class BuildFetcher:
+    """Consolidated list of failure infos generator."""
 
-    userinfo = userinfos[build.id]
-    if not build.match_filters(filters, userinfo):
-        return ("build", None)
+    def __init__(self, userinfos: userdata.UserInfos, limit: int,
+                 filters: dict[str, Any], preparelogs: bool = False):
+        self.userinfos = userinfos
+        self.limit = limit
+        self.filters = filters
+        self.preparelogs = preparelogs
+        self.__infos: list[swatbuild.Build] = []
 
-    return ("build", build)
+    def _create_build(self, buildid: int, failures: dict[int, dict]) -> None:
+        build = swatbuild.Build(buildid, failures)
 
+        userinfo = self.userinfos[build.id]
+        if not build.match_filters(self.filters, userinfo):
+            return
 
-def _prepare_log(build: swatbuild.Build):
-    logsview.get_log_highlights(build.get_first_failure(), "stdio")
-    return ("preparelogs", None)
+        self.__infos.append(build)
+        if self.preparelogs:
+            logsview.get_log_highlights(build.get_first_failure(), "stdio")
 
-
-def _create_builds(filters: dict[str, Any],
-                   failures: dict[int, dict[int, dict]],
-                   limit: int,
-                   userinfos: userdata.UserInfos,
-                   preparelogs: bool = False):
-    infos = []
-    limited_pending_ids = sorted(failures.keys(), reverse=True)[:limit]
-    jobs = []
-
-    # Generate a list of all pending failures, fetching details from the remote
-    # server as needed.
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    def _create_builds(self, failures: dict[int, dict[int, dict]],
+                       executor: utils.ExecutorWithProgress):
+        limited_pending_ids = sorted(failures.keys(),
+                                     reverse=True)[:self.limit]
+        # Generate a list of all pending failures, fetching details from the
+        # remote server as needed.
         for buildid in limited_pending_ids:
-            # Filter on status now, limiting the size of data we will have to
-            # download from the server.
-            if filters['triage']:
+            # Filter on status now, limiting the size of data we will have
+            # to download from the server.
+            if self.filters['triage']:
                 triages = {f['attributes']['triage']
                            for f in failures[buildid].values()}
 
-                if triages.isdisjoint(filters['triage']):
+                if triages.isdisjoint(self.filters['triage']):
                     continue
 
-            jobs.append(executor.submit(_create_build, filters, buildid,
-                                        failures[buildid], userinfos))
+            executor.submit("Fetching builds data", self._create_build,
+                            buildid, failures[buildid])
 
-        try:
-            progress = 0
-            jobcount = len(jobs) * (2 if preparelogs else 1)
-            with click.progressbar(length=jobcount,
-                                   label="Loading build failures details"
-                                   ) as jobsprogress:
-                for future in concurrent.futures.as_completed(jobs):
-                    restype, res = future.result()
-                    if restype == "build":
-                        if res is not None:
-                            infos.append(res)
-                            if preparelogs:
-                                jobs.append(executor.submit(_prepare_log, res))
-                                progress += 1
-                            else:
-                                progress += 2
-                        else:
-                            progress += 2
-                    elif restype == "preparelogs":
-                        progress += 1
+    def prepare_with_executor(self, executor: utils.ExecutorWithProgress):
+        """Prepare consolidated list of failure infos."""
+        statusfilter = None
+        if len(self.filters.get('triage', [])) == 1:
+            statusfilter = self.filters['triage'][0]
+        failures = swatbotrest.get_failures(statusfilter)
 
-                    jobsprogress.update(progress)
-        except KeyboardInterrupt:
-            executor.shutdown(cancel_futures=True)
-            return ([], userinfos)
-        except Exception:
-            executor.shutdown(cancel_futures=True)
-            raise
+        self._create_builds(failures, executor)
 
-    return infos
+    def prepare(self):
+        """Prepare consolidated list of failure infos."""
+        executor = utils.ExecutorWithProgress()
+        self.prepare_with_executor(executor)
+        executor.run()
 
+    def get_builds(self, sort: Collection[str],) -> list[swatbuild.Build]:
+        """Get consolidated list of failure infos."""
 
-def get_failure_infos(limit: int, sort: Collection[str],
-                      filters: dict[str, Any], preparelogs: bool = False
-                      ) -> tuple[list[swatbuild.Build], userdata.UserInfos]:
-    """Get consolidated list of failure infos and local reviews infos."""
-    userinfos = userdata.UserInfos()
+        def sortfn(elem):
+            return elem.get_sort_tuple([swatbuild.Field(k) for k in sort])
 
-    logger.info("Loading build failures...")
-
-    statusfilter = None
-    if len(filters.get('triage', [])) == 1:
-        statusfilter = filters['triage'][0]
-    failures = swatbotrest.get_failures(statusfilter)
-
-    infos = _create_builds(filters, failures, limit, userinfos, preparelogs)
-
-    def sortfn(elem):
-        return elem.get_sort_tuple([swatbuild.Field(k) for k in sort])
-
-    return (sorted(infos, key=sortfn), userinfos)
+        return sorted(self.__infos, key=sortfn)
